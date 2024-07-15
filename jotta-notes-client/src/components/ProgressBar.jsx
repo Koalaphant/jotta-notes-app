507c6f1ab9aac6e6e9e78dec4ab4909fd9107721
Koalaphant <andrew.wardjones@icloud.com>
2024-07-15 17:29:27 +0100

    added progress bars

--- jotta-notes-client/src/components/ProgressBar.jsx
+++ jotta-notes-client/src/components/ProgressBar.jsx
@@ -1,3 +1,10 @@
-export const ProgressBar = () => {
-  return <div></div>;
+export const ProgressBar = ({ progress }) => {
+  return (
+    <div className="outer-bar">
+      <div
+        className="inner-bar"
+        style={{ width: `${progress}%`, backgroundColor: "gray" }}
+      ></div>
+    </div>
+  );
 };
