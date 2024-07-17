872e33a2f7c97175a766265e3af0dee434c84227
Koalaphant <andrew.wardjones@icloud.com>
2024-07-17 12:55:13 +0100

    updated db information

--- jotta-notes-server/db.js
+++ jotta-notes-server/db.js
@@ -1,5 +1,8 @@
 const Pool = require("pg").Pool;
-require("dotenv").config();
+const dotenv = require("dotenv");
+
+const env = process.env.NODE_ENV || "development";
+dotenv.config({ path: `.env.${env}` });
 
 const pool = new Pool({
   user: process.env.VITE_API_USERNAME,
