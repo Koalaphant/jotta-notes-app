eb15448ab06e63c828eeebc4253ce4dba9b08cf2
Koalaphant <andrew.wardjones@icloud.com>
2024-07-17 17:04:40 +0100

    changed db connection

--- jotta-notes-server/db.js
+++ jotta-notes-server/db.js
@@ -1,15 +1,29 @@
-const Pool = require("pg").Pool;
+// const Pool = require("pg").Pool;
+// const dotenv = require("dotenv");
+
+// const env = process.env.NODE_ENV || "development";
+// dotenv.config({ path: `.env.${env}` });
+
+// const pool = new Pool({
+//   user: process.env.VITE_API_USERNAME,
+//   password: process.env.VITE_API_PASSWORD,
+//   host: process.env.VITE_API_HOST,
+//   port: process.env.VITE_API_DBPORT,
+//   database: "jottanotes",
+// });
+
+// module.exports = pool;
+
+const { Pool } = require("pg");
 const dotenv = require("dotenv");
 
-const env = process.env.NODE_ENV || "development";
-dotenv.config({ path: `.env.${env}` });
+dotenv.config(); // Load environment variables from .env file
 
 const pool = new Pool({
-  user: process.env.VITE_API_USERNAME,
-  password: process.env.VITE_API_PASSWORD,
-  host: process.env.VITE_API_HOST,
-  port: process.env.VITE_API_DBPORT,
-  database: "jottanotes",
+  connectionString: process.env.VITE_DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // Only use for development, remove for production
+  },
 });
 
 module.exports = pool;
